765603f48f0c737e424b8efd0565f04ed6db7a8c
Marcin Paszenda <marcinpaszenda@gmail.com>
2022-10-24 21:45:14 +0200

    32.3 Thymeleaf loop and conditional statement

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class MailCreatorService {
 
@@ -21,6 +24,12 @@ public class MailCreatorService {
     private TemplateEngine templateEngine;
 
     public String buildTrelloCardEmail(String message) {
+
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your tasks");
+        functionality.add("Provides connection with Trello Account");
+        functionality.add("Application allows sending tasks to Trello");
+
         Context context = new Context();
         context.setVariable("preview_message", "Welcome to Task Application! :)");
         context.setVariable("message", message);
@@ -29,6 +38,10 @@ public class MailCreatorService {
         context.setVariable("admin_name", adminConfig.getAdminName());
         context.setVariable("company_name", companyConfig.getCompanyName());
         context.setVariable("goodbye_message", "Have a nice day and good luck!");
+        context.setVariable("show_button", false);
+        context.setVariable("is_friend", false);
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("application_functionality", functionality);
 
         return templateEngine.process("mail/created-trello-card-mail", context);
 
